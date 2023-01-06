da029c9bc38fa9a5cf499a9ae9cde65d80e95143
Tchayo <achayof@gmail.com>
2023-01-07 00:59:08 +0300

    update messages

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Tchayo/var-replace/libs/message"
 	"github.com/Tchayo/var-replace/libs/template"
 )
 
 func main() {
+	messages := []string{"Message for the ages", "Now you know"}
 	// data structure the template will be applied to
 	vars := make(map[string]interface{})
 	vars["Name"] = "Brienne OBrien"
 	vars["Number"] = "0700000000"
-	vars["Message"] = []string{"Message for the ages", "Now you know"}
+	vars["Message"] = strings.Join(messages, ", ")
+
 
 	// process a template string
 	resultA := template.ProcessString("Hello {{.Name}}, Your mobile number is {{.Number}}. {{.Message}}", vars)
